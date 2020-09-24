85b82892c7bbdd7fad603d6ff2b3d73ed2250724
Y009 <henryj654@gmail.com>
2020-09-24 23:04:04 +0300

    Adding more info to CV page
    still more to add and fix pic. TODO: ADD modal to pic click

--- cv/projects.js
+++ cv/projects.js
@@ -35,9 +35,9 @@ var projects_data = {
   	},
     {
       name : "Riistvara projekt",
-      description : "Riistvara projekti eesmärgiks oli luua riistvaraline kauguse mõõtja. See oli esimene kokkupuude sardsüsteemidega ning see tõttu on projekt ka väga lihtne. Projekt kasutas TI MSP430 launchpad'i ja HC-SR04 ultrahelimoodulit. Projekt on loodud C's, kasutatud on TI DriveLibi ja dokumenteerimiseks Doxygeni.",
-      previewsrc : "https://camo.githubusercontent.com/90f5739ed3f5e7834fd8708dbc5320c8d337a7de/687474703a2f2f692e696d6775722e636f6d2f5a714d4f4c4d482e676966",
-      previewdesc : "Gif mängust",
+      description : "Riistvara projekti eesmärgiks oli luua riistvaraline kauguse mõõtja. See oli esimene kokkupuude sardsüsteemidega ning see tõttu on projekt ka väga lihtne. Projekt kasutas TI MSP430 launchpad'i, HC-SR04 ultrahelimoodulit ja LED ekraani. Projekt on loodud C's, kasutatud on TI DriveLibi ja dokumenteerimiseks Doxygeni.",
+      previewsrc : "https://i.imgur.com/PhvU3fc.jpg",
+      previewdesc : "Mõõtja kauguse numbriga",
       buttons : [
         {
           src : "https://github.com/Y009/IXX1420---Riistvara---projekt",
@@ -47,6 +47,85 @@ var projects_data = {
           src : "http://www.y009.eu/usproject/",
           desc : "Dokumentatsioon"
         }
+      ],
+      images : [
+        {
+          src : "https://i.imgur.com/U3u9gqs.jpg",
+          desc : "Mõõtja eestpoolt"
+        }
+               ]
+
+    },
+    {
+      name : "Baka lõputöö",
+      description : "Bakalaureuse töö eesmärgiks oli luua klassikalise Breakout mängu koopia mikrokontrolleri baasil, mille mänguväljaks said 4 LED maatriksit, shift registrite juhtimisel. Töös kasutatud alusena TI msp kontrollerit. Programmeerimise keeleks on C ja rakendatud on DriveLib library.",
+      previewsrc : "https://i.imgur.com/wZxlvbc.jpg",
+      previewdesc : "Sega-pundar LED maatriksitest ja juhtmetest",
+      buttons : [
+        {
+          src : "https://github.com/Y009/IXX1430---Arvutis-steemid---projekt",
+          desc : "Githubi link"
+        }
+                ],
+      images : [
+        {
+          src : "https://i.imgur.com/Ai7OBlo.jpg",
+          desc : "Sega-pundar LED maatriksitest ja juhtmetest. Nurk 2."
+        },
+        {
+          src : "https://lh3.googleusercontent.com/proxy/cLAGgCP7-DIXF_jiBdjvmAZUhgvkNqr8ji8RXbQ9BcSAzKVUpwp-X8NagAa5i6PsbQ0EOZn91I24-9LlIS_PIVME6f3BHtr62iQxJIGAaDr5aZdB",
+          desc : "Inspiratisoon originaal mängust"
+        },
+
                 ]
+
+    },
+    {
+      name : "Enginaator 2019",
+      description : "2019 aasta Enginaatoril eelvoorus oli ülesandeks STM32 arendusplaadi põhjal luua kell. Aega oli seal küll vähe aga millegagi saime hakkama. Siin olid komponendid ette antud, tuli need õigesti ja kiiresti kokku panna, ning peale seda luua samuti ka programne osa. \nTeine osa oli luua tuulegeneraator. See osa hõlmas rohkem ka muid teadmisi - magnetvälja abil elektri tootmine, kogu süsteemi disain ja füüsilise prototüübi kiire ehitamine, tuli mõõta ka tuuliku pöörlemiskiirust HALL'i sensori abil. See sai saavutatud kergesti kasutada antud Arduino kontrolleri abil.",
+      previewsrc : "../assets/img/enginaator_pic.jpg",
+      previewdesc : "2. koha pilt",
+      buttons : [
+        {
+          src : "https://github.com/Y009/IXX1430---Arvutis-steemid---projekt",
+          desc : "Githubi link"
+        }
+                ],
+      images : [
+        {
+          src : "https://i.imgur.com/Ai7OBlo.jpg",
+          desc : "Sega-pundar LED maatriksitest ja juhtmetest. Nurk 2."
+        },
+        {
+          src : "https://lh3.googleusercontent.com/proxy/cLAGgCP7-DIXF_jiBdjvmAZUhgvkNqr8ji8RXbQ9BcSAzKVUpwp-X8NagAa5i6PsbQ0EOZn91I24-9LlIS_PIVME6f3BHtr62iQxJIGAaDr5aZdB",
+          desc : "Inspiratisoon originaal mängust"
+        },
+
+                ]
+    },
+    {
+      name : "Magistri lõputöö",
+      description : "Magistritöö ülesandeks oli luua prototüüp õpirobot, mis hõlmaks paljusid erinevaid elemente. Robot sai loodud STM32 arendusplaadi põhjal ning hõlmab järgnevaid komponente: LCD, NFC arendusplaat, 3 servot, mootori kontroller, IMU, HALL'i sensor, WiFi moodul, ultraheli moodul, piezo kõlar ja LDO'dest koosnev pingeregulaator. Robot saab oma toite 6st akust/ või patareist. Roboti eesmärgiks on orienteeruda erinevaid komponente kasutades ette antud alal, lugeda sealt NFC siltidelt olevat infot, ning vahetada seda infot serveriga WiFi abil. Kuna tegemist oli õpiobjektiga siis oli parem kui on võimalik komponente üks haaval ise lisada, mitte lihtsalt ettevalmis loodud PCB'd lasta programmeerida. Lõpptulemus robotist kasutab FreeRTOS'i ja HAL librarysid.",
+      previewsrc : "https://i.imgur.com/wZxlvbc.jpg",
+      previewdesc : "Lõputöö, named Wall-e",
+      buttons : [
+        {
+          src : "https://digikogu.taltech.ee/et/Item/939e8050-497e-4042-9145-a8784ac87b91",
+          desc : "Töö digikogus"
+        }
+                ],
+      images : [
+        {
+          src : "https://i.imgur.com/Ai7OBlo.jpg",
+          desc : "Sega-pundar LED maatriksitest ja juhtmetest. Nurk 2."
+        },
+        {
+          src : "https://lh3.googleusercontent.com/proxy/cLAGgCP7-DIXF_jiBdjvmAZUhgvkNqr8ji8RXbQ9BcSAzKVUpwp-X8NagAa5i6PsbQ0EOZn91I24-9LlIS_PIVME6f3BHtr62iQxJIGAaDr5aZdB",
+          desc : "Inspiratisoon originaal mängust"
+        },
+
+                ]
+
     }
+
 ]};
--- js/prj.js
+++ js/prj.js
@@ -4,7 +4,6 @@ var projects_template;
 // and displays the results in the content div
 function showTemplate(template, data){
 	var html    = template(data);
-	console.log(html);
 	$('#content').html(html);
 }
 
@@ -12,12 +11,9 @@ function showTemplate(template, data){
 // is loaded, so we put most of the code that needs to run
 // in here
 $(document).ready(function(){
-console.log("doks ready1");
-	//
-	// compile all of our templates ready for use
-	//
+
 	var source   = $("#category-template").html();
 	projects_template = Handlebars.compile(source);
 	showTemplate(projects_template, projects_data);
-	console.log("doks ready2");
+
 });
